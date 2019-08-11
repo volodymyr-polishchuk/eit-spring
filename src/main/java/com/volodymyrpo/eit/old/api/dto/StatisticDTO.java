245cef65b470449dbab01fdefc2b0c5c35c3c61b
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-08-11 18:35:50 +0300

    Fix issue with database characters encoding, add password changing

--- src/main/java/com/volodymyrpo/eit/old/api/dto/StatisticDTO.java
+++ src/main/java/com/volodymyrpo/eit/old/api/dto/StatisticDTO.java
@@ -1,14 +1,7 @@
 package com.volodymyrpo.eit.old.api.dto;
 
-import lombok.Data;
-
-@Data
-public class StatisticDTO {
-    private String subject_name;
-    private Integer seconds;
-
-    public StatisticDTO(String subjectName, Integer seconds) {
-        this.subject_name = subjectName;
-        this.seconds = seconds;
-    }
+public interface StatisticDTO {
+    String getSubject_name();
+    Integer getSeconds();
+    String getFormatted_time();
 }
--- src/main/java/com/volodymyrpo/eit/old/api/dto/StatisticForDaysDTO.java
+++ src/main/java/com/volodymyrpo/eit/old/api/dto/StatisticForDaysDTO.java
@@ -1,9 +1,6 @@
 package com.volodymyrpo.eit.old.api.dto;
 
-import lombok.Data;
-
-@Data
-public class StatisticForDaysDTO {
-    private Integer day_result;
-    private String date;
+public interface StatisticForDaysDTO {
+    Integer getDay_result();
+    String getDate();
 }
