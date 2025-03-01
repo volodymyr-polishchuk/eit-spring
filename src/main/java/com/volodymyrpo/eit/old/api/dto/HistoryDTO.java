3482e5927da459d28de4470e0c4fae2d9add825a
Volodymyr Polishchuk <thevivalley@gmail.com>
2025-03-01 17:16:44 +0100

    Add get_history.php

--- src/main/java/com/volodymyrpo/eit/old/api/dto/HistoryDTO.java
+++ src/main/java/com/volodymyrpo/eit/old/api/dto/HistoryDTO.java
@@ -1,12 +1,9 @@
 package com.volodymyrpo.eit.old.api.dto;
 
-import lombok.Data;
-
-@Data
-public class HistoryDTO {
-    private String subjectName;
-    private String themeName;
-    private String dateStart;
-    private String dateEnd;
-    private Integer time;
+public interface HistoryDTO {
+    String getSubjectName();
+    String getThemeName();
+    String getDateStart();
+    String getDateEnd();
+    Integer getTime();
 }
